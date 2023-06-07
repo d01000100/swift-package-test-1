cb301ba72ca3f0b2e6f0c16882c1e2461a6f5a77
Daniel Casado <daniel.casado@frogdesign.com>
2023-06-07 16:30:08 -0600

    Add simple functions and enums

--- Sources/PackageTest1/PackageTest1.swift
+++ Sources/PackageTest1/PackageTest1.swift
@@ -1,6 +1,36 @@
+//import SwiftUI
+
 public struct PackageTest1 {
     public private(set) var text = "Hello, World!"
 
     public init() {
     }
+    
+    public enum Personality {
+        case polite
+        case energetic
+        case angsty
+    }
+    
+    public func greet (name: String, personality: Personality) -> String {
+        switch(personality) {
+        case .polite:
+            return "Greetings, \(name)"
+        case .energetic:
+            return "WHAT'S UP, \(name)!?"
+        case .angsty:
+            return "\(name).... whatev"
+        }
+    }
+    
+//    public struct Button: View {
+//        var name: String = "Default Text"
+//        var body: some View {
+//            Button {
+//                Text(name)
+//            }
+//        }
+//    }
 }
+
+!?
